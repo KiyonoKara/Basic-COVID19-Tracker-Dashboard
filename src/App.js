4b54efd06e267f45a28faa37b615747b9c3b9b18
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2023-10-27 18:24:01 -0400

    Feature: revise app and display all the data once the loading state is complete

--- src/App.js
+++ src/App.js
@@ -1,70 +1,73 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
 import logo from './Basic-COVID19-Tracker-Icon-Main.svg';
 import './App.css';
 
-const { getCOVIDData } = require('./APIData/index.js');
+const { getCovidData } = require('./api_data/index.js');
 const { formatNumber } = require('./components/index.js');
 
 class App extends React.Component {
     state = {
-        data: {
-            confirmed: {
-                value: 0
-            },
-            deaths: {
-                value: 0
-            },
-            recoveries: {
-                value: 0
-            }
-        }
+        confirmed: 0,
+        deaths: 0,
+        recoveries: 0,
+        loading: true
+
     }
     _isMounted = false;
 
     componentDidMount() {
         this._isMounted = true;
-        getCOVIDData().then(data => {
-            this.setState({ data });
+
+        getCovidData()
+            .then(data => {
+                if (this._isMounted) {
+                    this.setState({
+                        confirmed: data.confirmed,
+                        deaths: data.deaths,
+                        recoveries: data.recoveries,
+                        loading: false
+                    });
+                }
+            })
+            .catch(error => {
+            console.error('Error fetching data:', error);
+            if (this._isMounted) {
+                this.setState({ loading: false });
+            }
         });
     }
 
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
+
     render() {
-        let [rawConfirmed, rawDeaths, rawRecoveries] = ['', '', ''];
-        if (this.state.data.confirmed.value && this.state.data.deaths.value && this.state.data.recovered.value) {
-            rawConfirmed = this.state.data.confirmed.value;
-            rawDeaths = this.state.data.deaths.value;
-            rawRecoveries = this.state.data.recovered.value;
+        let { confirmed, deaths, recoveries, loading } = this.state;
+
+        if (loading) {
+            return <div>Loading...</div>;
         }
-        let [confirmed, deaths, recoveries] = [formatNumber(rawConfirmed), formatNumber(rawDeaths), formatNumber(rawRecoveries)];
+
         return (
           <div className="App">
               <header className="App-header">
                   <img src={logo} className="App-logo" alt="logo"/>
                   <p>
                       <b>
-                        Welcome to an extremely simple COVID-19 tracker
+                        COVID-19 Global Stats
                       </b>
                   </p>
                   <a className="App-link"
                      href="https://github.com/KiyonoKara/Basic-COVID19-Tracker-Dashboard"
                      target="_blank"
                      rel="noopener noreferrer">GitHub Source</a>
-                  <p>Global Cases: {confirmed}</p>
-                  <p>Global Deaths: {deaths}</p>
-                  <p>Global Recoveries: {recoveries}</p>
+                  <p>Cases: {formatNumber(confirmed)}</p>
+                  <p>Deaths: {formatNumber(deaths)}</p>
+                  <p>Recoveries: {formatNumber(recoveries)}</p>
               </header>
           </div>
       );
   }
-
-  componentWillUnmount() {
-        this._isMounted = false;
-    }
 }
-
-
-
-//ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
